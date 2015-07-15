ebf2f0f03ae915d01a463b910eeb9cb2f86fb163
Bruno Sant'Anna <mail@bsantanna.me>
2015-07-14 21:52:35 -0300

    Project kickoff

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lilivicApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lilivicApp
+ */
+angular.module('lilivicApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

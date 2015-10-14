b252d79ead038aa20cbb518cef0c74545ed46a73
Bruno Sant'Anna <mail@bsantanna.me>
2015-10-13 21:17:03 -0300

    Tratamento ui para enfase de onde o usuário está no site

--- project/app/scripts/controllers/navigation.js
+++ project/app/scripts/controllers/navigation.js
@@ -17,9 +17,30 @@ var labels = {
   }
 };
 
-angular.module('lilivicApp').controller('NavigationCtrl', function ($scope) {
+angular.module('lilivicApp').controller('NavigationCtrl', function ($scope, $location) {
+  $scope.currentI18n = 'pt_BR';
+
   $scope.setI18n = function (label) {
-    $scope.label = labels[label];
+    $scope.currentI18n = label;
+    $scope.label = labels[$scope.currentI18n];
   };
-  $scope.setI18n('pt_BR');
+
+  $scope.setI18n($scope.currentI18n);
+
+  $scope.labelLinkClass = function (label) {
+    var itemClass = '';
+    if (label === $scope.currentI18n) {
+      itemClass += 'selected';
+    }
+    return itemClass;
+  };
+
+  $scope.viewLinkClass = function (link) {
+    var itemClass = '';
+    if ('/' + link === $location.path()) {
+      itemClass += 'selected';
+    }
+    return itemClass;
+  };
+
 });

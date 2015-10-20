3ee2e1b8a5d841a68cf55b643124c8534ba972eb
Bruno Sant'Anna <mail@bsantanna.me>
2015-10-20 14:39:38 -0200

    protótipo animação

--- project/app/scripts/controllers/home.js
+++ project/app/scripts/controllers/home.js
@@ -1,5 +1,13 @@
 'use strict';
 
-angular.module('lilivicApp').controller('HomeCtrl', function ($rootScope) {
+angular.module('lilivicApp').controller('HomeCtrl', function ($rootScope, $scope, $timeout) {
+
+  $scope.panelClass = 'hide';
+
+  var callback = function () {
+    //$scope.panelClass = 'animation_test';
+  };
+  $timeout(callback, 3000);
+
   $rootScope.$broadcast(constants.EVENTS.VIEW_CHANGE);
 });

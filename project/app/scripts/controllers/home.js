423fe48efbdcdac08a871c0f0a7ab241b3515198
Bruno Sant'Anna <mail@bsantanna.me>
2015-10-20 16:26:12 -0200

    nova versão do site com efeito de fade in / fade out

--- project/app/scripts/controllers/home.js
+++ project/app/scripts/controllers/home.js
@@ -2,12 +2,30 @@
 
 angular.module('lilivicApp').controller('HomeCtrl', function ($rootScope, $scope, $timeout) {
 
-  $scope.panelClass = 'hide';
+  $scope.coverClass = 'hide';
+  $scope.coverCounter = 0;
 
-  var callback = function () {
-    $scope.panelClass = 'fade_in_animation home_cover_background_1';
+  var fadeOutEnd = function () {
+    $scope.coverClass = 'hide';
+    $timeout(fadeInStart(), 100);
   };
-  $timeout(callback, 3000);
+  var fadeOutStart = function () {
+    $scope.coverClass = 'fade_out_animation home_cover_background_' + $scope.coverCounter;
+    $timeout(fadeOutEnd, 950);
+  };
+
+  var fadeInStart = function () {
+    $scope.coverClass = 'hide';
+    $scope.coverCounter++;
+    if ($scope.coverCounter === 8) {
+      $scope.coverCounter = 1;
+    }
+    $scope.coverClass = 'fade_in_animation home_cover_background_' + $scope.coverCounter;
+
+    $timeout(fadeOutStart, 10000);
+  };
+
+  $timeout(fadeInStart, 100);
 
   $rootScope.$broadcast(constants.EVENTS.VIEW_CHANGE);
 });

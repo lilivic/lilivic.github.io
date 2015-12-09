4792e082a7a2e6ec153a698a44ea0c7588c75182
Bruno Sant'Anna <mail@bsantanna.me>
2015-12-09 17:19:58 -0200

    serviço i18n injetado nos controllers

--- project/app/scripts/controllers/about.js
+++ project/app/scripts/controllers/about.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('lilivicApp').controller('AboutCtrl', function ($rootScope) {
-
+angular.module('lilivicApp').controller('AboutCtrl', function ($rootScope, $scope, i18n) {
+  i18n.init($scope);
   $rootScope.$broadcast(constants.EVENTS.VIEW_CHANGE);
 });
--- project/app/scripts/controllers/works.js
+++ project/app/scripts/controllers/works.js
@@ -1,5 +1,6 @@
 'use strict';
 
-angular.module('lilivicApp').controller('WorksCtrl', function ($rootScope) {
+angular.module('lilivicApp').controller('WorksCtrl', function ($rootScope, $scope, i18n) {
+  i18n.init($scope);
   $rootScope.$broadcast(constants.EVENTS.VIEW_CHANGE);
 });

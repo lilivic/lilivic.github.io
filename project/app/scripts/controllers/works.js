26f291ddc037ff41d799589b8617dc3524744149
Bruno Sant'Anna <mail@bsantanna.me>
2015-12-10 16:17:54 -0200

    Protótipo modal

--- project/app/scripts/controllers/works.js
+++ project/app/scripts/controllers/works.js
@@ -1,6 +1,19 @@
 'use strict';
 
-angular.module('lilivicApp').controller('WorksCtrl', function ($rootScope, $scope, i18n) {
+angular.module('lilivicApp').controller('WorksCtrl', function ($rootScope, $scope, $modal, i18n) {
   i18n.init($scope);
+
+  $scope.open = function (image) {
+    $modal.open({
+      animation: true,
+      templateUrl: 'workModalContent.html',
+      resolve: {
+        image: function () {
+          return image;
+        }
+      }
+    });
+  };
+
   $rootScope.$broadcast(constants.EVENTS.VIEW_CHANGE);
 });

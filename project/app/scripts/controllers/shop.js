b7289fac762e11371f7d0d1f930d2af24a3cbe64
Bruno Sant'Anna <mail@bsantanna.me>
2015-12-16 12:31:17 -0200

    shop prototype

--- project/app/scripts/controllers/shop.js
+++ project/app/scripts/controllers/shop.js
@@ -1,6 +1,17 @@
 'use strict';
 
-angular.module('lilivicApp').controller('ShopCtrl', function ($rootScope, $scope, i18n) {
+angular.module('lilivicApp').controller('ShopCtrl', function ($rootScope, $scope, $routeParams, i18n) {
   i18n.init($scope);
+
+  $scope.currentSection = $routeParams.section;
+
+  $scope.isShopUnavailable = function (language) {
+    return $scope.getLanguage() === language;
+  };
+
+  $scope.isCurrentSection = function (section, language) {
+    return $scope.currentSection === section && $scope.getLanguage() === language;
+  };
+
   $rootScope.$broadcast(constants.EVENTS.VIEW_CHANGE);
 });
--- project/app/scripts/services/i18n.js
+++ project/app/scripts/services/i18n.js
@@ -9,6 +9,7 @@ angular.module('lilivicApp').service('i18n', function () {
       powered_by: 'Powered By',
       shop: 'Shop',
       shop_bags: 'Bags',
+      shop_bags_description: '?',
       shop_bodies: 'Baby bodies',
       shop_cushion_covers: 'Cushion covers',
       shop_mugs: 'Mugs',
@@ -28,13 +29,16 @@ angular.module('lilivicApp').service('i18n', function () {
       blog: 'Blog',
       shop: 'Shop',
       shop_bags: 'Bolsas',
+      shop_bags_description: 'Bolsas em Córdoba com alça em cadarço de algodão preto e impressão frente e verso. No modelo tote bag, com 40x43x5cm, não têm forro/fecho e são super resistentes.',
       shop_bodies: 'Macacões de bebê',
+      shop_bodies_description: 'Macacão de bebê 100% algodão, super macio, confortável e impresso com a alta tecnologia DTG de impressão direta sobre tecido com toque zero.',
       shop_cushion_covers: 'Capas de almofada',
       shop_mugs: 'Canecas',
       shop_phone_covers: 'Capas de celulares',
       shop_posters: 'Pôsters',
       shop_post_cards: 'Cartões postais',
       shop_shirts: 'Camisetas',
+      shop_shirts_description:'Camiseta Baby Long 100% algodão, em malha estonada ou quality, modelagem especial mais acinturada e comprida e impressão direta sobre o tecido, garantindo conforto e caimento perfeito. Ah, não desbota nem encolhe!',
       powered_by: 'Uma invenção',
       soon: 'Em breve',
       works: 'Galeria',
@@ -59,6 +63,7 @@ angular.module('lilivicApp').service('i18n', function () {
     init: function (scope) {
       scope.getMessage = this.getMessage;
       scope.setLanguage = this.setLanguage;
+      scope.getLanguage = this.getLanguage;
     },
     setLanguage: function (language) {
       currentLanguage = language;
